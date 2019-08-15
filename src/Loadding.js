a73dffbe191c75821c20fc21946ad2f3b7222957
王GG <s124168114@gmail.com>
2019-08-15 22:36:51 +0800

    新增 1.關鍵字 2.loadding畫面

---
+++ src/Loadding.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Loadding.css";
+const Loadding = () =>{
+  return(
+    <div className="recipe-load"> 
+      <h3></h3>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+      <img alt=""/>
+    </div>
+  )
+}
+export default Loadding;
