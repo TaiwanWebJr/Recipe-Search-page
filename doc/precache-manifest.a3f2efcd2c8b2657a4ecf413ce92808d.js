8b9e263845ff7d99ac92e523b6dd9f68883adfee
王GG <s124168114@gmail.com>
2019-10-23 23:26:05 +0800

    build in doc

---
+++ doc/precache-manifest.a3f2efcd2c8b2657a4ecf413ce92808d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32f7edd2fae571fbfd88b2f41d76636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81d836141a1b99d29ae",
+    "url": "/static/css/main.c5e5f9c1.chunk.css"
+  },
+  {
+    "revision": "16a6385f42917bcffd60",
+    "url": "/static/js/2.71412b09.chunk.js"
+  },
+  {
+    "revision": "e81d836141a1b99d29ae",
+    "url": "/static/js/main.88d7c246.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
